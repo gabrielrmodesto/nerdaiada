6836a452b4d7653e461e9dbdcf3c039e02847960
GabrielRM <gabrielrmodesto5@gmail.com>
2019-01-06 21:04:48 -0200

    Sweet alert implementado e estilizado

---
+++ js/principal.js
@@ -0,0 +1,9 @@
+var btn = document.querySelector("#btnEnvio");
+
+btn.addEventListener("click", function(){
+	swal({
+		title: "Muito obrigado",
+		text: "Logo irei responder sua mensagem",
+		button: "OK"
+	});
+}) 
