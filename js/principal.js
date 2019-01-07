7c33d13e59aa868c502a8f460eb4cefe054b26e2
GabrielRM <gabrielrmodesto5@gmail.com>
2019-01-06 23:41:51 -0200

    Função para scroll

--- js/principal.js
+++ js/principal.js
@@ -6,4 +6,26 @@ btn.addEventListener("click", function(){
 		text: "Logo irei responder sua mensagem",
 		button: "OK"
 	});
-}) 
+});
+
+const menuItens = document.querySelectorAll('.menu a');
+
+menuItens.forEach(item => {
+	item.addEventListener('click', scrollToId);
+});
+function getScrollToHref(element) {
+	const id = element.getAttribute('href');
+	return document.querySelector(id).offsetTop;
+}
+function scrollToId(event) {
+	event.preventDefault();
+	const section = getScrollToHref(event.target) - 50;
+	scrollToPosition(section);
+}
+
+function scrollToPosition(section){
+	window.scroll({
+		top: section,
+		behavior: "smooth"
+	});
+}
